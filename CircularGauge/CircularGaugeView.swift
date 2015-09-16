ffc0184bf8321eecbd7d5bed0a181c0a3d08f0af
dsk0701 <d.dubandtribal@gmail.com>
2015-09-17 01:29:38 +0900

    Update.

--- CircularGauge/CircularGaugeView.swift
+++ CircularGauge/CircularGaugeView.swift
@@ -12,42 +12,80 @@ import UIKit
 @IBDesignable
 class CircularGaugeView: UIView {
     
-    var lineWidth: CGFloat = 10.0
+    var lineWidth: CGFloat = 5.0
     var lineColor: UIColor = UIColor.orangeColor()
     var fillColor: UIColor = UIColor.clearColor()
-    var percentage: CGFloat = 1.0    // 0 〜 1
+    var percentage = 1.0    // 0 〜 1
+    var label: UILabel = UILabel()
+    var timer: NSTimer?
 
+    private let percentageFormat = "%.0f%%"
     private var circle: CAShapeLayer = CAShapeLayer()
+    private var animation: CABasicAnimation?
+    private var progress = 0.01
 
-    func drawCircle() {
-        
-        // 円の直径(枠線が frame より外に出ないように直径を算出)
-        let diameter = self.frame.size.width - lineWidth
-        
-        // 円の描画path設定
-        circle.path = UIBezierPath(roundedRect: CGRectMake(0, 0, diameter, diameter), cornerRadius: diameter / 2).CGPath
-        
-        // 円のポジション設定(枠線の太さ分位置を調整)
-        circle.position = CGPointMake(lineWidth / 2, lineWidth / 2)
-        
-        // 塗りの色を設定
-        circle.fillColor = fillColor.CGColor
-        
-        // 線の色を設定
-        circle.strokeColor = lineColor.CGColor
-        
-        // 線の幅を設定
-        circle.lineWidth = lineWidth
-        
-        // 該当のUIViewのlayerにsublayerとしてCALayerを追加
-        self.layer.addSublayer(circle)
-        
-        animate()
+    override func animationDidStart(anim: CAAnimation!) {
+
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "updateLabel", userInfo: nil, repeats: true)
     }
 
-    func animate() {
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        timer?.invalidate()
+        drawLabel(percentage)
+    }
 
-        // アニメーションkeyを設定
+    func updateLabel() {
+        progress += 0.01
+        let currentPercentage = progress * percentage
+        drawLabel(currentPercentage)
+    }
+
+    func draw() {
+
+        drawCircle()
+    }
+
+    func drawLabel(percentage: Double) {
+
+        let labelWidth = self.frame.width
+        label.frame = CGRectMake(0, 0, labelWidth, labelWidth)
+        label.center = CGPointMake(self.frame.width / 2, self.frame.width / 2)
+        label.text = String(format: percentageFormat, percentage * 100)
+        label.textAlignment = NSTextAlignment.Center
+        label.font = UIFont(name: "HiraKakuProN-W6", size: labelWidth / 5)
+        self.addSubview(label)
+    }
+
+    func drawCircle() {
+
+        // 円の直径(枠線が frame より外に出ないように直径を算出)
+        let diameter = self.frame.size.width - lineWidth
+
+        // 円の描画path設定
+        circle.path = UIBezierPath(roundedRect: CGRectMake(0, 0, diameter, diameter), cornerRadius: diameter / 2).CGPath
+
+        // 円のポジション設定(枠線の太さ分位置を調整)
+        circle.position = CGPointMake(lineWidth / 2, lineWidth / 2)
+
+        // 塗りの色を設定
+        circle.fillColor = fillColor.CGColor
+
+        // 線の色を設定
+        circle.strokeColor = lineColor.CGColor
+
+        // 線の幅を設定
+        circle.lineWidth = lineWidth
+
+        // 該当のUIViewのlayerにsublayerとしてCALayerを追加
+        animation = createAnimation()
+        circle.addAnimation(animation, forKey: "drawAnimation")
+
+        self.layer.addSublayer(circle)
+    }
+
+    private func createAnimation() -> CABasicAnimation {
+
+        // CAShapeLayer の strokeEnd プロパティをアニメーションさせます.
         let animation = CABasicAnimation(keyPath: "strokeEnd")
 
         // アニメーション間隔の指定
@@ -70,7 +108,9 @@ class CircularGaugeView: UIView {
         // 逆再生の指定
         animation.autoreverses = false
 
-        // Add the animation to the circle
-        circle.addAnimation(animation, forKey: "gaugeAnimation")
+        // デリゲート設定
+        animation.delegate = self
+
+        return animation
     }
 }
--- CircularGauge/ViewController.swift
+++ CircularGauge/ViewController.swift
@@ -10,27 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cgView = CircularGaugeView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let redView = UIView(frame: CGRectMake(0, 0, 100, 100))
-        redView.backgroundColor = UIColor.redColor()
-        redView.center = self.view.center
-        // self.view.addSubview(redView)
-        
-        let cgView = CircularGaugeView(frame: CGRectMake(0, 0, 100, 100))
-        cgView.center = self.view.center
-        cgView.percentage = 0.8
-        cgView.drawCircle()
-        self.view.addSubview(cgView)
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let frame = CGRectMake(0, 0, 80, 80)
+
+        let cgv1 = CircularGaugeView(frame: frame)
+        cgv1.center = CGPointMake(self.view.frame.width * 1 / 4, self.view.center.y)
+        cgv1.percentage = 0.2222
+        cgv1.lineColor = UIColor.orangeColor()
+        cgv1.draw()
+        self.view.addSubview(cgv1)
+
+        let cgv2 = CircularGaugeView(frame: frame)
+        cgv2.center = CGPointMake(self.view.frame.width * 2 / 4, self.view.center.y)
+        cgv2.percentage = 0.55555
+        cgv2.lineColor = UIColor.redColor()
+        cgv2.draw()
+        self.view.addSubview(cgv2)
+
+        let cgv3 = CircularGaugeView(frame: frame)
+        cgv3.center = CGPointMake(self.view.frame.width * 3 / 4, self.view.center.y)
+        cgv3.percentage = 0.88888
+        cgv3.lineColor = UIColor.purpleColor()
+        cgv3.draw()
+        self.view.addSubview(cgv3)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
